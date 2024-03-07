f297c58250f18c6bf8bfbfed688a1d907ed7d3bb
Akhil <akhilsoni1986@gmail.com>
2024-03-06 19:48:20 -0500

    added selenium tests

--- src/main/java/com/project/staragile/insureme/InsureMeApplication.java
+++ src/main/java/com/project/staragile/insureme/InsureMeApplication.java
@@ -1,13 +1,23 @@
 package com.project.staragile.insureme;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 @SpringBootApplication
 public class InsureMeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InsureMeApplication.class, args);
+		
 	}
 
 }
--- src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
+++ src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
@@ -2,8 +2,17 @@ package com.project.staragile.insureme;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 @SpringBootTest
 class InsureMeApplicationTests {
@@ -26,6 +35,27 @@ class InsureMeApplicationTests {
 		PolicyService pService = new PolicyService();
 		assertEquals(null,pService.searchPolicy());
 	}
+	
+	@Test
+	void testSelenium() throws InterruptedException {
+		  
+			WebDriverManager.chromedriver().setup();
+ 	
+	        ChromeOptions chromeOptions = new ChromeOptions();
+	        chromeOptions.addArguments("headless");
+	        chromeOptions.addArguments("--remote-allow-origins=*");
+	        WebDriver driver = new ChromeDriver(chromeOptions);
+	        driver.get("http://18.188.219.55:8081/");
+	        driver.findElement(By.linkText("Contact Us"));
+	        driver.findElement(By.id("inputName")).sendKeys("Capstone Assignment");
+	        driver.findElement(By.id("inputNumber")).sendKeys("1234567890");
+	        driver.findElement(By.id("inputMail")).sendKeys("test@test.com");
+	        driver.findElement(By.id("inputMessage")).sendKeys("This is my capstone assignment");
+	        driver.findElement(By.cssSelector(".submit_btn")).click();
+	        driver.quit();
+	        System.out.println("Submitted contact us form successfull");
+	}
+
 
 	
 }
